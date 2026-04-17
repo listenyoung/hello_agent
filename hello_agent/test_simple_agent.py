0643b2509d1ca031735e65815b4a7587457af8fe
listenyoung <13278014652@163.com>
2026-04-17 11:44:10 +0800

    Add files via upload
    first commit  继承SimpleAgent，将工具调用结果加入input传回LLM

---
+++ hello_agent/test_simple_agent.py
@@ -0,0 +1,5 @@
+# test_simple_agent.py
+from dotenv import load_dotenv
+from hello_agents import HelloAgentsLLM, ToolRegistry
+from hello_agents.tools import CalculatorTool
+from my_simple_agent import MySimpleAgent
